71d3979cce4b1a123694752603e7b0bebb96f741
yusupovarobiyakhon <robiyamuhidinova29@gmail.com>
2025-06-12 14:46:45 +0300

    add main

---
+++ main.py
@@ -0,0 +1,77 @@
+# i = 1
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+
+# n = int(input())
+# i = 1
+# s = 0
+# while i <= n:
+#     s += i
+#     i += 1
+# print(s)
+
+# n = int(input())
+# count = 0
+# if n == 0:
+#     count = 1
+# else:
+#     while n != 0:
+#         n //= 10
+#         count += 1
+# print(count)
+
+
+
+# i = 10
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+
+# n = 101
+# while n % 17 != 0:
+#     n += 1
+# print(n)
+
+
+
+# s = 0
+# while True:
+#     n = int(input())
+#     if n == 0:
+#         break
+#     s += n
+# print(s)
+
+
+
+# a = int(input())
+# b = int(input())
+# r = 1
+# i = 0
+# while i < b:
+#     r *= a
+#     i += 1
+# print(r)
+
+
+# a = int(input())
+# b = int(input())
+# while b != 0:
+#     a, b = b, a % b
+# print(a)
+
+
+# n = int(input())
+# for i in range(1, n + 1):
+#     sp = n - i
+#     st = 2 * i - 1
+#     print(' ' * sp + '*' * st)
+
+
+
+# n = int(input())
+# for i in range(1, n + 1):
+#     print((str(i) + ' ') * i)
